e33afdc1d34bf4fa2543594aac86195cada831dc
Manju KC <kcmanjuthapa@gmail.com>
2020-09-20 14:30:16 -0400

    created header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
+import Header from "./Component/Header"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        
         <Route path="/">
+          <Header/>
           <h1>HpmePage!!</h1>
         </Route>
       </Switch>
---
+++ src/Component/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <nav className="header">
+      <img  className="header_logo"
+      src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+      alt=""/>
+    </nav>
+  );
+}
+export default Header;
