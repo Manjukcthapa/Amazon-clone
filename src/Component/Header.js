4cd599201eeb858518aedbd74bca83b2aca41674
Manju KC <kcmanjuthapa@gmail.com>
2020-09-20 16:22:09 -0400

    added four links on nav

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -16,6 +17,34 @@ function Header() {
       <div className="header-search">
         <input type="text" className="header_searchInput" />
         <SearchIcon className="header_searchIcon" />
+      </div>
+
+      <div className="header_nav">
+        <Link  to="/logIn" className="header-link">
+          <div className="header_option">
+            <span className="option-one">Hello,</span>
+            <span className="option-two">Sign In</span>
+          </div>
+        </Link>
+        <Link className="header-link">
+          <div className="header_option">
+            <span className="option-one">Returns</span>
+            <span className="option-two">& Orders</span>
+          </div>
+        </Link>
+        <Link className="header-link">
+          <div className="header_option">
+            <span className="option-one">Your,</span>
+            <span className="option-two">Prime</span>
+          </div>
+        </Link>
+        <Link to="/checkout" className="header-link">
+          <div className="header-optionBasket">
+          <ShoppingBasketIcon className="shoppingBasketIcon" />
+              <span className="option-two basket_count">0</span>
+
+          </div>
+        </Link>
       </div>
     </nav>
   );
