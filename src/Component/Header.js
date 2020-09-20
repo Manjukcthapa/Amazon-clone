780264936e838b88488f94edfe2bb30a354e0a09
Manju KC <kcmanjuthapa@gmail.com>
2020-09-20 14:45:39 -0400

    created header css file and styled the logo

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,12 +1,18 @@
 import React from "react";
-
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <nav className="header">
-      <img  className="header_logo"
-      src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
-      alt=""/>
+      <Link>
+        <img
+          className="header_logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
+      
     </nav>
   );
 }
