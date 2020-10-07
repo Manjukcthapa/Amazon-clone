f5666979d7974469588a3773d6e5b906b2bf6e72
Manju KC <kcmanjuthapa@gmail.com>
2020-10-07 16:21:30 -0400

    updted amazon logo and changed the styling

--- src/Component/LogIn.js
+++ src/Component/LogIn.js
@@ -1,31 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./LogIn.css";
+
+
 function LogIn() {
   return (
     <div className="login">
       <Link to="/">
         <img
           className="login-logo"
-          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          src="https://thumbs.dreamstime.com/b/amazon-155631949.jpg"
           alt=""
         />
       </Link>
       <div className="login-container">
         <form>
-          <h1>Sign In</h1>
-          <h5>E-mail</h5>
-          <input type="text" />
-          <h5>Password</h5>
-          <input type="text" />
-          <button>Sign In</button>
+          <h1 className="header1">Sign In</h1>
+          <h5 className="header5">E-mail</h5>
+          <input className="inputs" type="text" />
+          <h5 className="header5">Password</h5>
+          <input  className="inputs" type="text" />
+          <button className="signin-button">Sign In</button>
         </form>
-        <p >
+        <p className="para">
           By Signing-in you agree to Amazoon's Conditions of see our Privacy
           Notice, our Cookies Notice, our Cookies and our interest-Based Add
           Notice.
         </p>
-        <button>Create Your Amazon Account</button>
+        <button className="register-button">Create Your Amazon Account</button>
       </div>
     </div>
   );
